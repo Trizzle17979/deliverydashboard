c68a87ea3781b95a5b9f0d3ec0f31e683b476176
Brett Smith <brettsmith212@gmail.com>
2022-02-06 18:18:41 -1000

    commented out Profile.tsx

--- src/components/loggedIn/Profile.tsx
+++ src/components/loggedIn/Profile.tsx
@@ -1,78 +1,78 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../../supabaseClient";
 
-const Profile = ({ session }) => {
+const Profile = () => {
   const [loading, setLoading] = useState(true);
   const [username, setUsername] = useState(null);
   const [website, setWebsite] = useState(null);
   const [avatar_url, setAvatarUrl] = useState(null);
 
-  useEffect(() => {
-    getProfile();
-  }, [session]);
+  // useEffect(() => {
+  //   getProfile();
+  // }, [session]);
 
-  async function getProfile() {
-    try {
-      setLoading(true);
-      const user = supabase.auth.user();
+  // async function getProfile() {
+  //   try {
+  //     setLoading(true);
+  //     const user = supabase.auth.user();
 
-      let { data, error, status } = await supabase
-        .from("profiles")
-        .select(`username, website, avatar_url`)
-        .eq("id", user.id)
-        .single();
+  //     let { data, error, status } = await supabase
+  //       .from("profiles")
+  //       .select(`username, website, avatar_url`)
+  //       .eq("id", user.id)
+  //       .single();
 
-      if (error && status !== 406) {
-        throw error;
-      }
+  //     if (error && status !== 406) {
+  //       throw error;
+  //     }
 
-      if (data) {
-        setUsername(data.username);
-        setWebsite(data.website);
-        setAvatarUrl(data.avatar_url);
-      }
-    } catch (error) {
-      alert(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }
+  //     if (data) {
+  //       setUsername(data.username);
+  //       setWebsite(data.website);
+  //       setAvatarUrl(data.avatar_url);
+  //     }
+  //   } catch (error) {
+  //     alert(error.message);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }
 
-  async function updateProfile({ username, website, avatar_url }) {
-    try {
-      setLoading(true);
-      const user = supabase.auth.user();
+  // async function updateProfile({ username, website, avatar_url }) {
+  //   try {
+  //     setLoading(true);
+  //     const user = supabase.auth.user();
 
-      const updates = {
-        id: user.id,
-        username,
-        website,
-        avatar_url,
-        updated_at: new Date(),
-      };
+  //     const updates = {
+  //       id: user.id,
+  //       username,
+  //       website,
+  //       avatar_url,
+  //       updated_at: new Date(),
+  //     };
 
-      let { error } = await supabase.from("profiles").upsert(updates, {
-        returning: "minimal", // Don't return the value after inserting
-      });
+  //     let { error } = await supabase.from("profiles").upsert(updates, {
+  //       returning: "minimal", // Don't return the value after inserting
+  //     });
 
-      if (error) {
-        throw error;
-      }
-    } catch (error) {
-      alert(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }
+  //     if (error) {
+  //       throw error;
+  //     }
+  //   } catch (error) {
+  //     alert(error.message);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }
 
   return (
     <div className="flex flex-col">
-      <div>
+      {/* <div>
         <label htmlFor="email">Email</label>
         <input id="email" type="text" value={session.user.email} disabled />
       </div>
-      <div>
-        <label htmlFor="username">Name</label>
+      <div> */}
+      {/* <label htmlFor="username">Name</label>
         <input
           id="username"
           type="text"
@@ -88,16 +88,16 @@ const Profile = ({ session }) => {
           value={website || ""}
           onChange={(e) => setWebsite(e.target.value)}
         />
-      </div>
+      </div> */}
 
       <div>
-        <button
+        {/* <button
           className=""
           onClick={() => updateProfile({ username, website, avatar_url })}
           disabled={loading}
         >
           {loading ? "Loading ..." : "Update"}
-        </button>
+        </button> */}
       </div>
 
       <div>
